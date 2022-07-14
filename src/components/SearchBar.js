282547ebe01dd318bb45f303094f138d153b7116
Joram Obbus <joramobbus@gmail.com>
2022-07-13 20:13:31 -0700

    updates

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,44 +1,19 @@
-import { useState } from "react";
+import { useState } from 'react'
+
 function SearchBar(props) {
-  let [searchTerm, setSearchTerm] = useState("");
+    let [searchTerm, setSearchTerm] = useState('')
 
-  const internalSearch = (e) => {
-    props.handleSearch(e, searchTerm);
-    // setSearchTerm("");
-  };
+    return (
+        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
 
-  return (
-    <form onSubmit={(e) => internalSearch(e)}>
-      <input
-        value={searchTerm}
-        id="search_bar"
-        type="text"
-        placeholder="Enter a search term here"
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      {/* <input type="submit" /> */}
-    </form>
-  );
+            <input type="text" placeholder="Enter a search term here" onChange={
+                (e) => setSearchTerm(e.target.value)
+            } />
+
+            <input type="submit" />
+
+        </form>
+    )
 }
 
-export default SearchBar;
-
-// import { useState } from "react";
-
-// function SearchBar(props) {
-//   // We can comment out our searchTerm state variable as we are not using it!
-//   // let [searchTerm, setSearchTerm] = useState('')
-
-//   return (
-//     <form>
-//       <input
-//         type="text"
-//         placeholder="Search Here"
-//         onChange={(e) => props.handleSearch(e, e.target.value)}
-//       />
-//       {/* <input type="submit" /> */}
-//     </form>
-//   );
-// }
-
-// export default SearchBar;
+export default SearchBar
