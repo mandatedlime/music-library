4604b9e453d915b0eaab1d4d21cee729337a1778
Joram Obbus <joramobbus@gmail.com>
2022-07-22 14:24:53 -0700

    updated search bar

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,14 +1,44 @@
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import {useEffect, useState} from 'react'
+import {useParams, Link, useHistory} from 'react-router-dom'
 
-function ArtistView() {
+const ArtistView = () => {
     const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
+    const history = useHistory()
+    
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const allAlbums = artistData.filter(entity => entity.collectionType === 'Album')
+                        .map((album, i) => { 
+                            return (
+                                <div key={i}>
+                                    <Link to={`/album/${album.collectionId}`}>
+                                        <p>{album.collectionName}</p>
+                                    </Link>
+                                </div>)
+                                })
+    
+    const navButtons = () => {
+        return (
+            <div>
+                <button onClick={() => {history.push('/')}}>Home</button> | <button onClick={() => history.goBack()}>Back</button>
+            </div>
+        )
+    }
 
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
+            {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <p>loading...</p>}
+            {navButtons()}
+            {allAlbums}
         </div>
     )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,15 @@
-
 import { useContext } from 'react'
 import { SearchContext } from '../context/SearchContext'
 
-function Searchbar() {
+const SearchBar = () => {
     const {term, handleSearch} = useContext(SearchContext)
 
     return (
-        <form >
-            <input ref={term} type="text" placeholder="Enter a search term here"/>
-            <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
-        </form>
+            <form>
+                <input ref={term} type="text" placeholder="Search Here" />
+                <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
+            </form>
     )
 }
 
-export default Searchbar
+export default SearchBar
